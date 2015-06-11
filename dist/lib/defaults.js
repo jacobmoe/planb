16e86bfae47faeab77f24a1ae4385db5597a6296
Jacob Moeller <jacobmoe@gmail.com>
2015-06-11 09:17:49 -0400

    remove dist from gitignore

---
+++ dist/lib/defaults.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var port = "5000";
+exports.port = port;
+var action = "get";
+exports.action = action;
+var allowedActions = ["get", "post", "put", "delete"];
+exports.allowedActions = allowedActions;
